1f0fe7b42caf24f735efc6fb73158acfa46c1d58
It's me bro <133140172+maihuyar@users.noreply.github.com>
2024-06-20 14:32:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class Bot(Client):
             app = web.AppRunner(await web_server())
             await app.setup()
             await web.TCPSite(app, "0.0.0.0", 8080).start()
-            logger.info("Web Response Is Running.....🕸️")
+            logger.info("Web Response Is Running....🕸️")
             
     async def stop(self, *args):
         await super().stop()
